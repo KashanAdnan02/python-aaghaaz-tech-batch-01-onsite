c65c3cf4d572ca9469d309af637ac30c10d8e157
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-15 02:40:20 -0700

    first class intro

---
+++ main.py
@@ -0,0 +1,44 @@
+# import math
+
+# math.
+
+
+print("Hello World")
+
+name = "Kashan"
+# Data Types:
+# String ""
+# Integer 121200138
+# Boolean True False
+# List [] ["Kashan", 12013 , True ,"Wasay"]
+# Dictionary {name : "Kashan"}
+# Sets {"kashan", "adnan"}
+print(name)
+
+
+
+x, y, z = 9, 8, 7
+
+print(x)
+print(y)
+print(z)
+x  = 10 + 12
+# x = 19
+print(x)
+
+# strring = "hello"/
+# strring = "Hello" + "2323"
+
+# students = ("kashan", " wasay")
+
+# students = ("hello")
+
+
+courses = ["web development", "helo"]
+
+courses2 = courses.copy()
+
+courses2[0] = "Helo"
+
+print(courses)
+print(courses2)
